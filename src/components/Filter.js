697d16dc039e98c8a7ff05e910bce5b1c53144ee
Wongsatorn Thongrod <thongrodw@gmail.com>
2024-11-09 16:07:28 +0700

    Update Filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -9,10 +9,11 @@ const Filter = ({ onFilterChange }) => {
     const [startDate, setStartDate] = useState(dayjs());
     const [endDate, setEndDate] = useState(dayjs());
     const [status, setStatus] = useState('Waiting');
+    const [period, setPeriod] = useState('Waiting');
 
     const handleFilterChange = () => {
         onFilterChange({ startDate, endDate, status });
-    }
+    };
 
     return (
         <Paper elevation={3} sx={{ padding: 2, marginBottom: 2, height: 'auto', maxWidth: 'xl', margin: '0 auto' }}>
@@ -24,48 +25,66 @@ const Filter = ({ onFilterChange }) => {
                 </Grid>
                 <Grid item xs={10}>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <Grid container spacing={2} alignItems="center" display='flex' justifyContent="flex-end" sx={{ marginLeft: 'auto' }}>
-                            <Grid item xs={12} md={2}>
-                                <TextField
-                                    select
-                                    label="Period"
-                                    value={status}
-                                    onChange={(e) => setStatus(e.target.value)}
-                                    fullWidth
-                                >
-                                    <MenuItem value="Waiting">Waiting</MenuItem>
-                                    <MenuItem value="Transmission">Transmission</MenuItem>
-                                </TextField>
+                        <Grid container spacing={2} alignItems="center" display="flex" justifyContent="flex-end" sx={{ marginLeft: 'auto' }}>
+                            <Grid item xs={12} md={2} container alignItems="center" spacing={1}>
+                                <Grid item>
+                                    <Typography variant="body1">Period</Typography>
+                                </Grid>
+                                <Grid item xs>
+                                    <TextField
+                                        select
+                                        value={period}
+                                        onChange={(e) => setPeriod(e.target.value)}
+                                        fullWidth
+                                        size="small"
+                                    >
+                                        <MenuItem value="Waiting">Waiting</MenuItem>
+                                        <MenuItem value="Transmission">Transmission</MenuItem>
+                                    </TextField>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={2}>
-                                <TextField
-                                    select
-                                    label="Status"
-                                    value={status}
-                                    onChange={(e) => setStatus(e.target.value)}
-                                    fullWidth
-                                >
-                                    <MenuItem value="Waiting">Waiting</MenuItem>
-                                    <MenuItem value="Transmission">Transmission</MenuItem>
-                                </TextField>
+                            <Grid item xs={12} md={2} container alignItems="center" spacing={1}>
+                                <Grid item>
+                                    <Typography variant="body1">Status</Typography>
+                                </Grid>
+                                <Grid item xs>
+                                    <TextField
+                                        select
+                                        value={status}
+                                        onChange={(e) => setStatus(e.target.value)}
+                                        fullWidth
+                                        size="small"
+                                    >
+                                        <MenuItem value="Waiting">Waiting</MenuItem>
+                                        <MenuItem value="Transmission">Transmission</MenuItem>
+                                    </TextField>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={2}>
-                                <DatePicker
-                                    label="From"
-                                    value={startDate}
-                                    onChange={(date) => setStartDate(date)}
-                                    renderInput={(params) => <TextField {...params} fullWidth />}
-                                />
+                            <Grid item xs={12} md={2} container alignItems="center" spacing={1}>
+                                <Grid item>
+                                    <Typography variant="body1">From</Typography>
+                                </Grid>
+                                <Grid item xs>
+                                    <DatePicker
+                                        value={startDate}
+                                        onChange={(date) => setStartDate(date)}
+                                        slotProps={{ textField: { size: 'small' } }}
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={2}>
-                                <DatePicker
-                                    label="To"
-                                    value={endDate}
-                                    onChange={(date) => setEndDate(date)}
-                                    renderInput={(params) => <TextField {...params} fullWidth />}
-                                />
+                            <Grid item xs={12} md={2} container alignItems="center" spacing={1}>
+                                <Grid item>
+                                    <Typography variant="body1">To</Typography>
+                                </Grid>
+                                <Grid item xs>
+                                    <DatePicker
+                                        value={endDate}
+                                        onChange={(date) => setEndDate(date)}
+                                        slotProps={{ textField: { size: 'small' } }}
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={1} sx={{ display: 'flex', justifyContent: 'flex-end', paddingRight: 2}}>
+                            <Grid item xs={12} md={1} sx={{ display: 'flex', justifyContent: 'flex-end', paddingRight: 1 }}>
                                 <Button variant="contained" color="primary" onClick={handleFilterChange}>
                                     Search
                                 </Button>
